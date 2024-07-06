6747bd1f999f9d1d286d25b7c4d98b715e5d25ac
Cristopher <cristopherquintanilla20060@gmail.com>
2024-07-06 15:50:03 -0600

    Codelab: Persistencia en Memoria con H2

---
+++ Spring-Boot-With-H2-Database-cristopher/src/main/java/com/example/Spring/Boot/With/H2/Database/cristopher/SpringBootWithH2DatabaseCristopherApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.With.H2.Database.cristopher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithH2DatabaseCristopherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithH2DatabaseCristopherApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-With-H2-Database-cristopher/src/test/java/com/example/Spring/Boot/With/H2/Database/cristopher/SpringBootWithH2DatabaseCristopherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.With.H2.Database.cristopher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithH2DatabaseCristopherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
